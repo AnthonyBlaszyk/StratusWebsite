9af19411ed95ac6cfbb7861278496b8e0be36143
Anthony <blaszyk.anthony@gmail.com>
2022-07-29 18:41:43 +0200

    feat: first commit

---
+++ pages/components/VideoBackround.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const VideoBackground = () => {
+  return (
+    <div>
+      <video src="/assets/stratusVideo.mp4" autoPlay muted />
+    </div>
+  );
+};
+
+export default VideoBackground;
