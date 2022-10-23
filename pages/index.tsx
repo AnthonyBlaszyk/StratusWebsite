e54335c3f2bc79eca1e1ccce1ba6746ebdc29072
Anthony <blaszyk.anthony@gmail.com>
2022-10-23 10:52:52 +0200

    feat: reset and add bootstrap

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import IndexNav from '../components/index/indexNav'
-import VideoBackground from '../components/VideoBackround'
 
 const Home: NextPage = () => {
   return (
@@ -11,12 +9,6 @@ const Home: NextPage = () => {
         <meta name="description" content="Stratus, une identité visuelle à votre image" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <div className="main">
-        <VideoBackground/>
-        <h1>Soon...</h1>
-        <IndexNav/>
-      </div>
     </div>
   )
 }
--- pages/subscription.tsx
+++
@@ -1,9 +0,0 @@
-import  Layout  from "../components/layout/layout.component"
-import type { NextPage } from 'next'
-import SubscriptionComponent  from "../components/subscription/subscription.component"
-
-const Home: NextPage = () => {
-  return Layout(SubscriptionComponent())
-}
-
-export default Home
